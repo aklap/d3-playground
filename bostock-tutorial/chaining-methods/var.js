24333014227e49900763653723b6161ee7049883
Alexis La Porte <aklaporte@gmail.com>
2018-06-14 23:26:26 -0400

    Use var keyword for selections.

---
+++ bostock-tutorial/chaining-methods/var.js
@@ -0,0 +1,10 @@
+// 'Since method chaining can only be used to descend into the document
+// hierarchy, use var to keep references to selections and go back up.'
+
+var section = d3.selectAll('section');
+
+section.append('div')
+       .html('But even Nikos, who could turn cats into cattle, snowflakes into snowdrops, and unicorns into men, could not change me into so much as a carnival cardsharp.');
+
+section.append('div')
+       .html("A last he said to me, 'My son, your ineptitude is so vast, your incompetence so profound, that I am certain you are inhabited by greater power than I have ever known. '");
