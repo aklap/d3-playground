b616728ff87537b9b01f85791fcc5a635558eb55
Alexis La Porte <aklaporte@gmail.com>
2018-09-16 00:53:29 -0400

    Fetch dataset.

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
 var url =
 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json';
+var dataset;
 
 // Append an SVG for our bar chart
 d3.select('body')
   .append('svg');
+
+// Fetch data for dataset as json with key-value of year and GDP in
+// billions of USD $
+d3.json(url, function(err, data) {
+  if(err) {
+    console.log(err);
+  }
+
+  dataset = JSON.parse(data);
+});
 
