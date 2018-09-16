39ad1ac4f3daaa38743c68e79cea9e8fc0c19e49
Alexis La Porte <aklaporte@gmail.com>
2018-09-16 00:57:54 -0400

    Add width and height to SVG.

--- main.js
+++ main.js
@@ -2,9 +2,14 @@ var url =
 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json';
 var dataset;
 
+var w = 900,
+    h = 460;
+
 // Append an SVG for our bar chart
 d3.select('body')
-  .append('svg');
+  .append('svg')
+  .attr('width', 900)
+  .attr('height', 460);
 
 // Fetch data for dataset as json with key-value of year and GDP in
 // billions of USD $
