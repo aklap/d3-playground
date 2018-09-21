ffe856dd389e27704ea0939d246715ef87d80458
Alexis La Porte <aklaporte@gmail.com>
2018-09-20 20:13:23 -0400

    Create new value label and transition into place on click.

--- scott-murray-tutorial/ch-9/bar-chart/main.js
+++ scott-murray-tutorial/ch-9/bar-chart/main.js
@@ -73,68 +73,67 @@ svg.selectAll('text')
 
 d3.select('p')
    .on('click', function() {
-      var numValues = dataset.length,
-          maxValue = 100;
+      var maxValue = 25;
+      var newNumber = Math.floor(Math.random() * maxValue);
+      dataset.push(newNumber);
 
-      dataset = [];
-
-      // Randomize the data
-      for(var i = 0; i < numValues; i++) {
-         var newNumber = Math.floor(Math.random() * maxValue);
-         dataset.push(newNumber);
-      }
-
-      yScale.domain([0, d3.max(dataset)]); // Scale bars with new data
+      xScale.domain(d3.range(dataset.length));
 
       // Update bars
-      svg.selectAll('rect') // updates the values of the bars
-         .data(dataset)
-         .transition() // animates a transition to new chart, default
-         // is 250ms. NOTE: Remember transitions can only be done on
-         // exisiting values!
-         .delay(function(d, i) {
-            return i / dataset.length * 1000; // NOTE: this will mean
-            // the total animation
-            // time will be longer than 500!
-         })// can take a numeric val as ms or anonymous function
-         .duration(500) // NOTE: must always be after transition
-         .ease(d3.easeCubicInOut)
-         .attr('y', function(d) { // updates the height of the bars
+      var bars = svg.selectAll('rect')
+                     .data(dataset);
+
+      bars.enter()
+          .append('rect')
+          .attr('x', w)
+          .attr('y', function(d) {
             return h - yScale(d);
-         })
-         .attr('height', function(d) {
+          })
+          .attr('width', xScale.bandwidth())
+          .attr('height', function(d) {
             return yScale(d);
-         })
-         .attr('fill', function(d) {
-            return 'rgb(0, 0, ' + Math.round(d * 10) + ')';
-         });
+          })
+          .attr('fill', function(d) {
+            return 'rgb(0, 0,' + Math.round(d * 10) + ')';
+          })
+          .merge(bars)
+          .transition()
+          .duration(500)
+          .attr('x', function(d, i) {
+            return xScale(i);
+          })
+          .attr('y', function(d) {
+            return h - yScale(d);
+          })
+          .attr('width', xScale.bandwidth())
+          .attr('height', function(d) {
+            return yScale(d);
+          });
 
       // Update labels
-      svg.selectAll('text')
-         .data(dataset)
-         .transition() // So labels transition smoothly in sync with
-         // bars
-         .duration(2000)
-         .text(function(d) {
-            return d;
-         })
-         .attr('x', function(d, i) {
-            return xScale(i) + xScale.bandwidth() / 2;
-         })
-         .attr('y', function(d) {
-            if(d <= 1) {
-               return h - yScale(d) - 2;
-            } else {
-               return h - yScale(d) + 14; // what is this magic num??
-            }
-         })
-         .attr('fill', function(d, i) {
-            if(d <= 1) {
-               return 'rgb(0, 0, 0)';
-            } else {
-               return 'white';
-            }
-         });
+      var labels = svg.selectAll('text').data(dataset);
+
+      labels.enter()
+            .append('text')
+            .text(function(d) {
+               return d;
+            })
+            .merge(labels)
+            .transition()
+            .duration(2000)
+            .text(function(d) {
+               return d;
+            })
+            .attr('x', function(d, i) {
+               return xScale(i) + xScale.bandwidth() / 2;
+            })
+            .attr('y', function(d) {
+               return h - yScale(d) + 14;
+            })
+            .attr('font-family', 'sans-serif')
+            .attr('font-size', '11px')
+            .attr('fill', 'white')
+            .attr('text-anchor', 'middle');
    });
 
 
