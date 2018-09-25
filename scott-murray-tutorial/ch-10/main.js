f22dd3d58222ab36adc0c50be16bbb00f8e71829
Alexis La Porte <aklaporte@gmail.com>
2018-09-25 19:09:46 -0400

    Sort bars and labels.

--- scott-murray-tutorial/ch-10/main.js
+++ scott-murray-tutorial/ch-10/main.js
@@ -1,5 +1,25 @@
-var dataset = [ 5, 10, 13, 19, 21, 25, 22, 18, 15, 13,
-              11, 12, 15, 20, 18, 17, 16, 18, 23, 25 ];
+var dataset = [
+               { key: 0, value: 5 },
+               { key: 1, value: 10 },
+               { key: 2, value: 13 },
+               { key: 3, value: 19 },
+               { key: 4, value: 21 },
+               { key: 5, value: 25 },
+               { key: 6, value: 22 },
+               { key: 7, value: 18 },
+               { key: 8, value: 15 },
+               { key: 9, value: 13 },
+               { key: 10, value: 11 },
+               { key: 11, value: 12 },
+               { key: 12, value: 15 },
+               { key: 13, value: 20 },
+               { key: 14, value: 18 },
+               { key: 15, value: 17 },
+               { key: 16, value: 16 },
+               { key: 17, value: 18 },
+               { key: 18, value: 23 },
+               { key: 19, value: 25 }
+            ];
 
 // Set dimensions of svg
 var w = 600,
@@ -18,7 +38,7 @@ var xScale = d3.scaleBand() // creates an ordinal scale
 
 var yScale = d3.scaleLinear()
                .domain([0, d3.max(dataset, function(d) {
-                  return d;
+                  return d.value;
                })])
                .range([0, h]);
 
@@ -31,14 +51,14 @@ var yScale = d3.scaleLinear()
       return xScale(i); //
    }) // prevents bars from overlapping -- spaces evenly
    .attr('y', function(d) {
-      return h - yScale(d); // turns bar right side up
+      return h - yScale(d.value); // turns bar right side up
    })
    .attr('width', xScale.bandwidth())
    .attr('height', function(d) {
-      return yScale(d);
+      return yScale(d.value);
    })
    .attr('fill', function(d) {
-      return "rgb(0, 0, " + (d * 10) + ")";
+      return "rgb(0, 0, " + (d.value * 10) + ")";
    })
    .on('click', function() {
       sortBars();
@@ -50,13 +70,13 @@ svg.selectAll('text')
    .enter()
    .append('text')
    .text(function(d) {
-      return d;
+      return d.value;
    })
    .attr('x', function(d, i) {
       return xScale(i) + xScale.bandwidth() / 2;
    }) // center in the middle of the bar
    .attr('y', function(d) {
-      return h - yScale(d) + 14;
+      return h - yScale(d.value) + 14;
    })
    .attr('font-family', 'sans-serif')
    .attr('font-size', '11px')
@@ -68,13 +88,23 @@ svg.selectAll('text')
 var sortBars = function() {
    svg.selectAll('rect')
       .sort(function(a, b) {
-         return d3.ascending(a, b);
+         return d3.ascending(a.value, b.value);
       })
       .transition()
       .duration(1000)
       .attr('x', function(d, i) {
          return xScale(i);
       });
+
+   svg.selectAll('text')
+      .sort(function(a, b) {
+         return d3.ascending(a.value, b.value);
+      })
+      .transition()
+      .duration(1000)
+      .attr('x', function(d, i) {
+         return xScale(i) + xScale.bandwidth() / 2;
+      })
 };
 
 
